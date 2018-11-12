c6bb75ffd286c1148af37bc8f96581179811d415
Zach Nusbaum <zach@zdnmail.com>
2018-11-12 15:02:31 -0800

    Bootstrapify the data preview

--- src/app/geolocation/geocode-results/geocode-results.component.ts
+++ src/app/geolocation/geocode-results/geocode-results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import * as fromGeolocation from '../reducers/geolocation.reducer';
+import { ResetCoordinates } from '../actions/geolocation.actions';
 
 @Component({
   selector: 'app-geocode-results',
@@ -13,6 +14,10 @@ export class GeocodeResultsComponent implements OnInit {
   constructor(private store: Store<fromGeolocation.State>) { }
 
   ngOnInit() {
+  }
+
+  reset() {
+    this.store.dispatch(new ResetCoordinates());
   }
 
 }

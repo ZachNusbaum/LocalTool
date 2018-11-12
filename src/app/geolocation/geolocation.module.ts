ca7776bf6e92b32c33e7cd063fc273ebc43c653c
Zach Nusbaum <zach@zdnmail.com>
2018-11-12 12:47:26 -0800

    Move locator component to correct module subfolder

--- src/app/geolocation/geolocation.module.ts
+++ src/app/geolocation/geolocation.module.ts
@@ -1,4 +1,4 @@
-import { LocatorComponent } from './../locator/locator.component';
+import { LocatorComponent } from './../geolocation/locator/locator.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';

cc97f285667fa6d52c39357818304d344bd11114
Zach Nusbaum <zach@zdnmail.com>
2018-11-12 12:46:11 -0800

    Add locator component

--- src/app/geolocation/geolocation.module.ts
+++ src/app/geolocation/geolocation.module.ts
@@ -1,13 +1,17 @@
+import { LocatorComponent } from './../locator/locator.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import * as fromGeolocation from './reducers/geolocation.reducer';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LocatorComponent
+  ],
   imports: [
     CommonModule,
     StoreModule.forFeature('geolocation', fromGeolocation.reducer)
-  ]
+  ],
+  exports: [LocatorComponent]
 })
 export class GeolocationModule { }

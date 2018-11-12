37a2b58e34b53ab6545507789749b7798997a4a3
Zach Nusbaum <zach@zdnmail.com>
2018-11-12 15:43:35 -0800

    App geocodes automatically when site is loaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeocodeResultsComponent } from './geolocation/geocode-results/geocode-results.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: GeocodeResultsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { State } from './reducers/index';
+import { FetchCoordinates } from './geolocation/actions/geolocation.actions';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
+import { takeUntil, first, delay } from 'rxjs/operators';
+import * as fromGeolocation from './geolocation/reducers/geolocation.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'LocalTool';
+export class AppComponent implements OnInit, OnDestroy {
+  geolocation$ = this.store$.select('geolocation');
+  private unsubscribe: Subject<void> = new Subject();
+
+  constructor(private store$: Store<State>) {}
+
+  ngOnInit() {
+    this.store$.dispatch(new FetchCoordinates());
+  }
+
+  ngOnDestroy() {
+
+  }
 }

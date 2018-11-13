29ebe728d15d2bdfa302687f1b6e4992b21c7f24
Zach Nusbaum <zach@zdnmail.com>
2018-11-12 18:10:30 -0800

    Begin air quality effects

--- src/app/air-quality/effects/air-quality.effects.ts
+++ src/app/air-quality/effects/air-quality.effects.ts
@@ -1,14 +1,21 @@
+import { mergeMap, tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
 import { AirQualityActionTypes } from '../actions/air-quality.actions';
+import { AirvisualService } from '../airvisual.service';
 
 @Injectable()
 export class AirQualityEffects {
 
-  @Effect()
+  @Effect({dispatch: false})
   loadAirQuality$ = this.actions$.pipe(
-    ofType(AirQualityActionTypes.LoadAirQuality)
+    ofType(AirQualityActionTypes.LoadAirQuality),
+    mergeMap((action: any) => {
+      return this.air.get(action.latitude, action.longitude);
+    },
+    tap((response: any) => console.log(response))
+  )
   );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private air: AirvisualService) {}
 }
